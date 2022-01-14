bdc9b660a546340e7a460524d3aef22d6590a4c5
Aquos <31460865+AquosTheWolf@users.noreply.github.com>
2022-01-14 01:00:10 -0500

    Why am I pushing agaon so soon? Because I'm tired thats why. So dont complain, random stranger.

--- client.py
+++ client.py
@@ -3,3 +3,19 @@ from typing import List, Callable, Dict, Union
 
 import asyncio
 import config
+import datetime
+import discord
+import log
+import os
+import prefix
+import sys
+import time
+import traceback
+
+class FrameworkClient(discord.Client):
+    __version__ = "0.1"
+    _background_tasks: List[Callable[[], None]] = []
+    _ready_handlers: List[Callable[[], None]] = []
+    _shutdown_handlers: List[Callable[[], None]] = []
+    _message_handlers: List[Callable[[discord.Message], None]] = []
+    _member_join_handlers: List[Callable[[discord.Member], None]] = []
